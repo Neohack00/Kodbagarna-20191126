9e1c9bc8a6f55da08653af57c166b1a46ce6b788
Philip Rutberg <philip.rutberg@edu.huddinge.se>
2019-11-26 11:53:50 +0100

    Changed port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.get('/', function(req, res){
     res.sendfile(__dirname + '/Public/Webb/index.html');
 });
 
-var server = app.listen(1337, function(){
+var server = app.listen(process.env.PORT, function(){
     console.log('Server is up and running on port ' + server.address().port);
 })
