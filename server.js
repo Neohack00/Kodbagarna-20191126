b0110eb10a47bfcd92c43d31ef6a504c1736bae2
Philip Rutberg <philip.rutberg@edu.huddinge.se>
2019-11-26 11:51:16 +0100

    Created local server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/Public/Webb/index.html');
+});
+
+var server = app.listen(1337, function(){
+    console.log('Server is up and running on port ' + server.address().port);
+})
