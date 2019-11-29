56eca09581a0cadde062c878500afec4547979d7
Simpan1001 <simon.springer@edu.huddinge.se>
2019-11-29 11:38:48 +0100

    Added static

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+//var bodyParser = require('body-parser');
+//var urlendcoderParser = bodyParser.urlencoded();
+
+app.use(express.static('Public'))
+
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/Public/Webb/index.html');
 });
 
-var server = app.listen(process.env.PORT, function(){
+var server = app.listen(1000, function(){
     console.log('Server is up and running on port ' + server.address().port);
-})
+});
